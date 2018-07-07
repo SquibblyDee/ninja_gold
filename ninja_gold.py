d7bc76a97c8252cea682a449087813d6c3807fe2
ste ste <steelst0@gmail.com>
2018-07-07 14:41:20 -0700

    textarea now defaults to bottom

--- ninja_gold.py
+++ ninja_gold.py
@@ -2,15 +2,13 @@ from flask import Flask, render_template, redirect, session, request
 app = Flask(__name__)
 app.secret_key='these_exercises_are_slowly_killing_me'
 import random
-import time
 
 @app.route('/')
 def index():
     if 'gold' not in session:
         session['gold'] = 0
         session['building'] = ""
-        session['activity'] = []
-        session['i'] = 0
+        session['activity'] = ""
     return render_template('ninja_gold.html')
 
 @app.route('/destroy_session')
@@ -29,19 +27,12 @@ def process_money():
         goldEarned = random.randint(2,5)
     else:
         goldEarned = random.randint(-50,50)
-    # print("GOLD EARNED", goldEarned)
+    print("GOLD EARNED", goldEarned)
     session['gold']+= goldEarned
     if goldEarned > 0:
-        session['activity'].append("Earned "+str(goldEarned)+" gold from the "+session['building']+"!\n")
-        print("INDEX ",session['i'])
-        session['i']+=1
-        print("INDEX2 ",session['i'])
-        for i in range(len(session['activity'])):
-            print(session['activity'][i])
+        session['activity']+="Earned "+str(goldEarned)+" gold from the "+session['building']+"! \n"
     else:
-        session['activity'].append("Entered a "+session['building']+" and lost "+str(goldEarned)+" gold... Ouch...\n")
-        for i in range(len(session['activity'])):
-            print(session['activity'][i])
+        session['activity']+="Entered a "+session['building']+" and lost "+str(goldEarned)+" gold... Ouch... \n"
     return redirect('/')
 
 if __name__ == '__main__':
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,9 @@
-// alert('ohai flask');
+function alerting(){
+    alert('ohai flask');
+}
+
+
+function forceTextToBottom(){
+    var textarea = document.getElementById('text_output');
+    textarea.scrollTop = textarea.scrollHeight;
+}

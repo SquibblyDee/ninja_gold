3b6a83d37fec9c68dde7229526df896849b93232
ste ste <steelst0@gmail.com>
2018-07-07 01:50:36 -0700

    functional but uggo

--- ninja_gold.py
+++ ninja_gold.py
@@ -7,10 +7,17 @@ import random
 def index():
     return render_template('ninja_gold.html')
 
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return redirect('/')
+
 @app.route('/process_money', methods=['POST'])
 def process_money():
     if 'gold' not in session:
         session['gold'] = 0
+        session['building'] = ""
+        session['activity'] = ""
     session['building'] = request.form['building']
     if session['building'] == 'farm':
         goldEarned = random.randint(10,20)
@@ -22,10 +29,11 @@ def process_money():
         goldEarned = random.randint(-50,50)
     print("GOLD EARNED", goldEarned)
     session['gold']+= goldEarned
-    session['activity'] = "Earned "+str(goldEarned)+" gold from the "+session['building']+"!"
+    if goldEarned > 0:
+        session['activity']+="Earned "+str(goldEarned)+" gold from the "+session['building']+"! \n"
+    else:
+        session['activity']+="Entered a "+session['building']+" and lost "+str(goldEarned)+" gold... Ouch... \n"
     return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    if session['gold'] > 0:
-        session['gold']=0

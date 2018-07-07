57e89c57633da25dd9bb868f5f16dbb7e86e25dd
ste ste <steelst0@gmail.com>
2018-07-07 16:15:03 -0700

    fixing the things I broke during cleanup

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,5 @@
 //This forces our textarea window to the bottom to show player updates as they happen.
 function forceTextToBottom(){
-    var textarea = document.getElementsByClassName('form-control');
+    var textarea = document.getElementById('text_output');
     textarea.scrollTop = textarea.scrollHeight;
 }
